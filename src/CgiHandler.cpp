db021e998bee43f312c5abbe185f38be9070fff9
João Lucas <joao_carlos1995@hotmail.com>
2025-07-24 18:12:22 +0100

    Implementation of CGI wip

--- src/CgiHandler.cpp
+++ src/CgiHandler.cpp
@@ -19,21 +19,24 @@ CgiHandler::~CgiHandler(){
 
 void    CgiHandler::initEnv(HttpRequest request){
 	env["AUTH_TYPE"] = "";
-	env["CONTENT_LENGHT"] = request.headerInfo["content-lenght"];
-	env["CONTENT_TYPE"] = request.headerInfo["content-type"];
+	env["CONTENT_LENGHT"] = request.headerInfo["Content-lenght"];
+	env["CONTENT_TYPE"] = request.headerInfo["Content-type"];
 	env["GATEWAY_INTERFACE"] = "CGI/1.1";
 	env["PATH_INFO"] = 
 	env["PATH_TRANSLATED"] = 
 	env["QUERY_STRING"] = decodeQuery(request);
-	env["REMOTE_ADDR"] = request.headerInfo["host"];
-	env["REMOTE_HOST"] = 
-	env["REMOTE_IDENT"] = 
-	env["REMOTE_USER"] = 
-	env["REQUEST_METHOD"] = 
+	env["REMOTE_ADDR"] = request.headerInfo["Host"];
+	env["REQUEST_METHOD"] = request.method;
 	env["SCRIPT_NAME"] = 
-	env["SERVER_NAME"] = request.headerInfo["host"].substr(0, request.headerInfo["host"].find(":"));
-	env["SERVER_PORT"] = request.headerInfo["host"].substr(request.headerInfo["host"].find(":") + 1, request.headerInfo["host"].size());
-	env["SERVER_PROTOCOL"] = 
-	env["SERVER_SOFTWARE"] = 
-	env["HTTP_COOKIE"] = 
+	env["SERVER_NAME"] = request.headerInfo["Host"].substr(0, request.headerInfo["Host"].find(":"));
+	env["SERVER_PORT"] = request.headerInfo["Host"].substr(request.headerInfo["Host"].find(":") + 1, request.headerInfo["host"].size());
+	env["SERVER_PROTOCOL"] = request.version;
+	env["SERVER_SOFTWARE"] = SERVER_VERSION;
+	env["HTTP_COOKIE"] = request.headerInfo["Cookie"];
+}
+
+std::string	CgiHandler::decodeQuery(HttpRequest request)
+{
+	std::string	toDecode = request.path.substr(request.path.find("?"), request.path.size());
+	
 }
